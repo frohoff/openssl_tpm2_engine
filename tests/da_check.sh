f1d70279839c206eb8fab4e58ff03d134823b745
James Bottomley <James.Bottomley@HansenPartnership.com>
2018-01-22 17:02:04 -0800

    tests: add dest for DA protection flag
    Simple test that tries to sign a message using an incorrect key
    password until the TPM locks out.  Once this happens verify that the
    correct password also gives an error, reset the DA lock on the TPM and
    verify that the operation proceeds normally.
    
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

---
+++ tests/da_check.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+bindir=${srcdir}/..
+
+##
+# test is
+# 1. create TPM internal private key with a password and da protection
+# 2. loop trying to sign a message with the wrong password until lockout occurs
+# 3. verify that correct password is still locked out
+# 4. reset the tpm DA lock
+# 5. verify that correct password works
+${bindir}/create_tpm2_key --rsa --da -a -k passw0rd key.tpm || exit 1
+count=1;
+while true; do
+    echo "This is a message" | 
+    openssl rsautl -sign -engine tpm2 -keyform engine -inkey key.tpm -passin pass:passwrd -out tmp.msg 2>tmp.txt
+    val=$?
+    cat tmp.txt
+    if [ $val -ne 1 ]; then
+	echo "Sign with incorrect key returned $val"
+	exit 1;
+    fi
+    if grep -q TPM_RC_LOCKOUT tmp.txt; then
+	break;
+    fi
+    # The TPM can return RETRY instead of AUTH_FAIL if it is still writing
+    # the DA state to NV ram
+    grep -q TPM_RC_AUTH_FAIL tmp.txt || grep -q TPM_RC_RETRY tmp.txt|| exit 1
+    count=$[$count+1]
+done
+echo "Locked out after $count tries"
+# try with correct password, should still be locked out
+echo "This is a message" | 
+openssl rsautl -sign -engine tpm2 -keyform engine -inkey key.tpm -passin pass:passw0rd -out tmp.msg 2>tmp.txt
+val=$?
+cat tmp.txt
+if [ $val -ne 1 ]; then
+    echo "Try with correct password did not fail correctly: $val"
+    exit 1;
+fi
+grep -q TPM_RC_LOCKOUT tmp.txt || exit 1
+# clear the TPM DA (this would normally be password protected)
+tssdictionaryattacklockreset
+echo "This is a message" | 
+openssl rsautl -sign -engine tpm2 -keyform engine -inkey key.tpm -passin pass:passw0rd -out tmp.msg || exit 1
