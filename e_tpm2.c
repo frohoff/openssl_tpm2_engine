frohoff/openssl_tpm2_engine

commit a5dab4a412b800462b62092579ccedfc4b7de9e6

e_tpm2: Don't keep TPM key resident for the openssl key lifetime
Originally e_tpm2 was created as a test for the in-kernel TPM2
resource manager, which means it was supposed to generate situations
where multiple keys were simultaneously in use.  It did this by
loading the TPM version of the key as soon as the openssl key was
loaded and keeping it resident until the openssl key was destroyed
(which can be hours or days depending on the application).  However,
this means that the engine really doesn't work without a resource
manager.  Fix this by making the time the engine keeps the key loaded
as short as possible, making it possible to use the engine on Linux
Kernel versions before 4.12 (the version that introduced the TPM2
resource manager).

Note that this fix is probabalistic: it is still possible for multiple
engine users to try to load keys simultaneously and get a failure
because the TPM is out of resources, but it makes the window where
this can happen much shorter.

Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

e_tpm2.c
@@ -167,12 +167,11 @@ void tpm2_bind_key_to_engine(EVP_PKEY *pkey, void *data)
 	}
 }
 
-static const char *tpm2_set_unique_tssdir(TSS_CONTEXT *tssContext)
+static const char *tpm2_set_unique_tssdir(void)
 {
 	char *prefix = getenv("XDG_RUNTIME_DIR"), *template,
 		*dir;
 	int len = 0;
-	TPM_RC rc;
 
 	if (!prefix)
 		prefix = "/tmp";
@@ -188,9 +187,6 @@ static const char *tpm2_set_unique_tssdir(TSS_CONTEXT *tssContext)
 	len = snprintf(template, len, "%s/tss2.XXXXXX", prefix);
 
 	dir = mkdtemp(template);
-	rc = TSS_SetProperty(tssContext, TPM_DATA_DIR, dir);
-	if (rc)
-		tpm2_error(rc, "TSS_SetProperty TPM_DATA_DIR");
 	return dir;
 }
 
@@ -198,11 +194,8 @@ static int tpm2_engine_load_key_core(ENGINE *e, EVP_PKEY **ppkey,
 				     const char *key_id,  BIO *bio,
 				     struct tpm_ui *ui, void *cb_data)
 {
-	Load_In in;
-	Load_Out out;
-	TSS_CONTEXT *tssContext;
-	TPM_RC rc;
 	EVP_PKEY *pkey;
+	TPM2B_PUBLIC p;
 	BIO *bf;
 	TSSLOADABLE *tssl;
 	BYTE *buffer;
@@ -259,67 +252,46 @@ static int tpm2_engine_load_key_core(ENGINE *e, EVP_PKEY **ppkey,
 		goto err;
 	}
 
-	app_data = OPENSSL_malloc(sizeof(struct app_data));
+	app_data = OPENSSL_malloc(sizeof(*app_data));
 
 	if (!app_data) {
 		fprintf(stderr, "Failed to allocate app_data\n");
 		goto err;
 	}
+	memset(app_data, 0, sizeof(*app_data));
 
-	rc = TSS_Create(&tssContext);
-	if (rc) {
-		tpm2_error(rc, "TSS_Create");
-		goto err_free;
-	}
-	app_data->dir = tpm2_set_unique_tssdir(tssContext);
-	app_data->tssContext = tssContext;
+	app_data->dir = tpm2_set_unique_tssdir();
 
-	app_data->parent = 0;
 	if (tssl->parent)
 		app_data->parent = ASN1_INTEGER_get(tssl->parent);
 
-	if (app_data->parent == 0)
-		tpm2_load_srk(tssContext, &app_data->parent, srk_auth, NULL);
-
-	in.parentHandle = app_data->parent;
 	empty_auth = tssl->emptyAuth;
 
-	buffer = tssl->privkey->data;
-	size = tssl->privkey->length;
-	TPM2B_PRIVATE_Unmarshal(&in.inPrivate, &buffer, &size);
-	buffer = tssl->pubkey->data;
-	size = tssl->pubkey->length;
-	TPM2B_PUBLIC_Unmarshal(&in.inPublic, &buffer, &size, FALSE);
+	app_data->priv = OPENSSL_malloc(tssl->privkey->length);
+	if (!app_data->priv)
+		goto err_free;
+	app_data->priv_len = tssl->privkey->length;
+	memcpy(app_data->priv, tssl->privkey->data, app_data->priv_len);
 
+	app_data->pub = OPENSSL_malloc(tssl->pubkey->length);
+	if (!app_data->pub)
+		goto err_free;
+	app_data->pub_len = tssl->pubkey->length;
+	memcpy(app_data->pub, tssl->pubkey->data, app_data->pub_len);
+	buffer = app_data->pub;
+	size = app_data->pub_len;
+	TPM2B_PUBLIC_Unmarshal(&p, &buffer, &size, FALSE);
 	/* create the new objects to return */
-	pkey = tpm2_to_openssl_public(&in.inPublic.publicArea);
+	pkey = tpm2_to_openssl_public(&p.publicArea);
 	if (!pkey) {
 		fprintf(stderr, "Failed to allocate a new EVP_KEY\n");
 		goto err_free;
 	}
 
-	rc = TSS_Execute(tssContext,
-			 (RESPONSE_PARAMETERS *)&out,
-			 (COMMAND_PARAMETERS *)&in,
-			 NULL,
-			 TPM_CC_Load,
-			 TPM_RS_PW, NULL, 0,
-			 TPM_RH_NULL, NULL, 0,
-			 TPM_RH_NULL, NULL, 0,
-			 TPM_RH_NULL, NULL, 0);
-
-	if (rc) {
-		tpm2_error(rc, "TPM2_Load");
-		goto err_free_key;
-	}
-
-	app_data->key = out.objectHandle;
-
-	app_data->auth = NULL;
 	if (empty_auth == 0) {
 		app_data->auth = tpm2_get_auth(ui, "TPM Key Password: ", cb_data);
 		if (!app_data->auth)
-			goto err_unload;
+			goto err_free_key;
 	}
 
 	TSSLOADABLE_free(tssl);
@@ -329,8 +301,6 @@ static int tpm2_engine_load_key_core(ENGINE *e, EVP_PKEY **ppkey,
 	*ppkey = pkey;
 	return 1;
 
- err_unload:
-	tpm2_flush_handle(tssContext, app_data->key);
  err_free_key:
 	EVP_PKEY_free(pkey);
  err_free:
@@ -397,19 +367,75 @@ static int tpm2_bind_fn(ENGINE * e, const char *id)
 	return 1;
 }
 
+TPM_HANDLE tpm2_load_key(TSS_CONTEXT **tsscp, struct app_data *app_data)
+{
+	TSS_CONTEXT *tssContext;
+	Load_In in;
+	Load_Out out;
+	TPM_HANDLE key = 0;
+	TPM_RC rc;
+	BYTE *buffer;
+	INT32 size;
+
+	rc = TSS_Create(&tssContext);
+	if (rc) {
+		tpm2_error(rc, "TSS_Create");
+		return 0;
+	}
+
+	buffer = app_data->priv;
+	size = app_data->priv_len;
+	TPM2B_PRIVATE_Unmarshal(&in.inPrivate, &buffer, &size);
+
+	buffer = app_data->pub;
+	size = app_data->pub_len;
+	TPM2B_PUBLIC_Unmarshal(&in.inPublic, &buffer, &size, FALSE);
+
+	if (app_data->parent) {
+		in.parentHandle = app_data->parent;
+	} else {
+		rc = tpm2_load_srk(tssContext, &in.parentHandle, NULL, NULL);
+		if (rc)
+			goto out;
+	}
+	rc = TSS_Execute(tssContext,
+			 (RESPONSE_PARAMETERS *)&out,
+			 (COMMAND_PARAMETERS *)&in,
+			 NULL,
+			 TPM_CC_Load,
+			 TPM_RS_PW, NULL, 0,
+			 TPM_RH_NULL, NULL, 0);
+	if (rc)
+		tpm2_error(rc, "TPM2_Load");
+	else
+		key = out.objectHandle;
+
+	if (!app_data->parent)
+		tpm2_flush_srk(tssContext, in.parentHandle);
+ out:
+	if (!key)
+		TSS_Delete(tssContext);
+	else
+		*tsscp = tssContext;
+	return key;
+}
+
+void tpm2_unload_key(TSS_CONTEXT *tssContext, TPM_HANDLE key)
+{
+	tpm2_flush_handle(tssContext, key);
+
+	TSS_Delete(tssContext);
+}
+
 void tpm2_delete(struct app_data *app_data)
 {
-	tpm2_flush_srk(app_data->tssContext, app_data->parent);
-
-	if (app_data->tssContext)
-		TSS_Delete(app_data->tssContext);
+	OPENSSL_free(app_data->priv);
+	OPENSSL_free(app_data->pub);
 
 	if (rmdir(app_data->dir) < 0)
 		perror("Unlinking TPM_DATA_DIR");
 
-
-	if (app_data->dir)
-		OPENSSL_free((void *)app_data->dir);
+	OPENSSL_free((void *)app_data->dir);
 
 	OPENSSL_free(app_data);
 }

e_tpm2-ecc.c
@@ -76,9 +76,8 @@ static TPM_HANDLE tpm2_load_key_from_ecc(const EC_KEY *eck, TSS_CONTEXT **tssCon
 		return 0;
 
 	*auth = app_data->auth;
-	*tssContext = app_data->tssContext;
 
-	return app_data->key;
+	return tpm2_load_key(tssContext, app_data);
 }
 
 void tpm2_bind_key_to_engine_ecc(EVP_PKEY *pkey, void *data)
@@ -110,8 +109,6 @@ static void tpm2_ecc_free(void *parent, void *ptr, CRYPTO_EX_DATA *ad,
 
 	if (!data)
 		return;
-
-	tpm2_flush_handle(data->tssContext, data->key);
 
 	tpm2_delete(data);
 }
@@ -163,9 +160,11 @@ static ECDSA_SIG *tpm2_ecdsa_sign(const unsigned char *dgst, int dgst_len,
 	in.validation.tag = TPM_ST_HASHCHECK;
 	in.validation.hierarchy = TPM_RH_NULL;
 	in.validation.digest.t.size = 0;
+
+	sig = NULL;
 	rc = tpm2_get_hmac_handle(tssContext, &authHandle, 0);
 	if (rc)
-		return NULL;
+		goto out;
 
 	rc = TSS_Execute(tssContext,
 			 (RESPONSE_PARAMETERS *)&out,
@@ -177,12 +176,12 @@ static ECDSA_SIG *tpm2_ecdsa_sign(const unsigned char *dgst, int dgst_len,
 	if (rc) {
 		tpm2_error(rc, "TPM2_Sign");
 		tpm2_flush_handle(tssContext, authHandle);
-		return NULL;
+		goto out;
 	}
 
 	sig = ECDSA_SIG_new();
 	if (!sig)
-		return NULL;
+		goto out;
 
 	r = BN_bin2bn(out.signature.signature.ecdsa.signatureR.t.buffer,
 		      out.signature.signature.ecdsa.signatureR.t.size,
@@ -197,6 +196,8 @@ static ECDSA_SIG *tpm2_ecdsa_sign(const unsigned char *dgst, int dgst_len,
 #else
 	ECDSA_SIG_set0(sig, r, s);
 #endif
+ out:
+	tpm2_unload_key(tssContext, in.keyHandle);
 	return sig;
 }
 
@@ -213,6 +214,7 @@ static int tpm2_ecc_compute_key(unsigned char **psec, size_t *pseclen,
 	const EC_GROUP *group;
 	BN_CTX *ctx;
 	unsigned char point[MAX_ECC_KEY_BYTES*2 + 1];
+	int ret;
 
 	group = EC_KEY_get0_group(eck);
 	ctx = BN_CTX_new();
@@ -236,9 +238,10 @@ static int tpm2_ecc_compute_key(unsigned char **psec, size_t *pseclen,
 	memcpy(in.inPoint.point.y.t.buffer, point + 1 + len, len);
 	in.inPoint.point.y.t.size = len;
 
+	ret = 0;
 	rc = tpm2_get_hmac_handle(tssContext, &authHandle, 0);
 	if (rc)
-		return 0;
+		goto out;
 
 	rc = TSS_Execute(tssContext,
 			 (RESPONSE_PARAMETERS *)&out,
@@ -250,12 +253,12 @@ static int tpm2_ecc_compute_key(unsigned char **psec, size_t *pseclen,
 	if (rc) {
 		tpm2_error(rc, "TPM2_ECDH_ZGen");
 		tpm2_flush_handle(tssContext, authHandle);
-		return 0;
+		goto out;
 	}
 
 	*psec = OPENSSL_malloc(len);
 	if (!*psec)
-		return 0;
+		goto out;
 	*pseclen = len;
 	memset(*psec, 0, len);
 
@@ -263,7 +266,10 @@ static int tpm2_ecc_compute_key(unsigned char **psec, size_t *pseclen,
 	memcpy(*psec + len - out.outPoint.point.x.t.size,
 	       out.outPoint.point.x.t.buffer,
 	       out.outPoint.point.x.t.size);
-	return 1;
+	ret = 1;
+ out:
+	tpm2_unload_key(tssContext, in.keyHandle);
+	return ret;
 }
 
 #if OPENSSL_VERSION_NUMBER < 0x10100000

e_tpm2-rsa.c
@@ -108,9 +108,7 @@ static TPM_HANDLE tpm2_load_key_from_rsa(RSA *rsa, TSS_CONTEXT **tssContext, cha
 		return 0;
 
 	*auth = app_data->auth;
-	*tssContext = app_data->tssContext;
-
-	return app_data->key;
+	return tpm2_load_key(tssContext, app_data);
 }
 
 void tpm2_bind_key_to_engine_rsa(EVP_PKEY *pkey, void *data)
@@ -135,14 +133,9 @@ static void tpm2_rsa_free(void *parent, void *ptr, CRYPTO_EX_DATA *ad,
 			  int idx, long argl, void *argp)
 {
 	struct app_data *app_data = ptr;
-	TSS_CONTEXT *tssContext;
 
 	if (!app_data)
 		return;
-
-	tssContext = app_data->tssContext;
-
-	tpm2_flush_handle(tssContext, app_data->key);
 
 	tpm2_delete(app_data);
 }
@@ -172,7 +165,7 @@ static int tpm2_rsa_priv_dec(int flen,
 	rv = -1;
 	if (padding != RSA_PKCS1_PADDING) {
 		fprintf(stderr, "Non PKCS1 padding asked for\n");
-		return rv;
+		goto out;
 	}
 
 	in.inScheme.scheme = TPM_ALG_RSAES;
@@ -182,7 +175,7 @@ static int tpm2_rsa_priv_dec(int flen,
 
 	rc = tpm2_get_hmac_handle(tssContext, &authHandle, 0);
 	if (rc)
-		return rv;
+		goto out;
 
 	rc = TSS_Execute(tssContext,
 			 (RESPONSE_PARAMETERS *)&out,
@@ -195,14 +188,15 @@ static int tpm2_rsa_priv_dec(int flen,
 		tpm2_error(rc, "TPM2_RSA_Decrypt");
 		/* failure means auth handle is not flushed */
 		tpm2_flush_handle(tssContext, authHandle);
-		return rv;
+		goto out;
 	}
  
 	memcpy(to, out.message.t.buffer,
 	       out.message.t.size);
 
 	rv = out.message.t.size;
-
+ out:
+	tpm2_unload_key(tssContext, in.keyHandle);
 	return rv;
 }
 
@@ -220,6 +214,11 @@ static int tpm2_rsa_priv_enc(int flen,
 	char *auth;
 	TPM_HANDLE authHandle;
 
+	if (padding != RSA_PKCS1_PADDING) {
+		fprintf(stderr, "Non PKCS1 padding asked for\n");
+		return -1;
+	}
+
 	in.keyHandle = tpm2_load_key_from_rsa(rsa, &tssContext, &auth);
 
 	if (in.keyHandle == 0) {
@@ -229,14 +228,9 @@ static int tpm2_rsa_priv_enc(int flen,
 	}
 
 	rv = -1;
-	if (padding != RSA_PKCS1_PADDING) {
-		fprintf(stderr, "Non PKCS1 padding asked for\n");
-		return rv;
-	}
-
 	rc = tpm2_get_hmac_handle(tssContext, &authHandle, 0);
 	if (rc)
-		return rv;
+		goto out;
 
 	/* this is slightly paradoxical that we're doing a Decrypt
 	 * operation: the only material difference between decrypt and
@@ -262,13 +256,16 @@ static int tpm2_rsa_priv_enc(int flen,
 		tpm2_error(rc, "TPM2_RSA_Decrypt");
 		/* failure means auth handle is not flushed */
 		tpm2_flush_handle(tssContext, authHandle);
-		return rv;
+		goto out;
 	}
 
 	memcpy(to, out.message.t.buffer,
 	       out.message.t.size);
 
 	rv = out.message.t.size;
+
+ out:
+	tpm2_unload_key(tssContext, in.keyHandle);
 
 	return rv;
 }

e_tpm2.h
@@ -7,15 +7,18 @@
 #define TPM2_ENGINE_EX_DATA_UNINIT		-1
 
 /* structure pointed to by the RSA object's app_data pointer */
-struct app_data
-{
-	TSS_CONTEXT *tssContext;
+struct app_data {
 	TPM_HANDLE parent;
-	TPM_HANDLE key;
+	void *priv;
+	int priv_len;
+	void *pub;
+	int pub_len;
 	char *auth;
 	const char *dir;
 };
 
+TPM_HANDLE tpm2_load_key(TSS_CONTEXT **tsscp, struct app_data *app_data);
+void tpm2_unload_key(TSS_CONTEXT *tssContext, TPM_HANDLE key);
 void tpm2_delete(struct app_data *app_data);
 
 #endif

tests/fail_connect.sh
@@ -34,13 +34,29 @@ pg9RqI0tA1DtJPyRSVNWvK9488KBdXa9rgHncDl5krAnsoA+k3B7hZyk8vPsKg6I
 eHE/E4MuHfTbmc56LxxPf7g5rNrGG+Aamu+DRg==
 -----END TSS2 KEY BLOB-----
 " > tmp.tpm
+##
+# conversion to public key doesn't actually contact the TPM
+# so this should succeed
+##
 openssl rsa -engine tpm2 -inform engine -in tmp.tpm -pubout -out tmp.pub 2> tmp.txt
+if [ $? -ne 0 ]; then
+    echo "TPM key import failed with $?"
+    cat tmp.txt
+    exit 1
+fi
+##
+# key operation does contact the TPM and should fail
+##
+echo "This is a message" |\
+openssl rsautl -sign -engine tpm2 -keyform engine -inkey tmp.tpm -out tmp.msg 2> tmp.txt
 if [ $? -ne 1 ]; then
-    # error other than 1 usually means segfault
-    exit 1;
+    echo "TPM key signing failed with $?"
+    cat tmp.txt
+    exit 1
 fi
 cat tmp.txt
-grep -q TSS_RC_NO_CONNECTION tmp.txt || exit 1
 grep -q 'TPM2_Load failed' tmp.txt || exit 1
+grep -q TSS_RC_NO_CONNECTION tmp.txt || exit 1
+
 rm -f tmp.tpm tmp.txt
 

commit ec76e5f42ef8633614e40888078fb5e824020161

e_tpm: bug fix: set the TSS directory
Setting the key store directory was a property of the engine prior to

commit a5dab4a412b800462b62092579ccedfc4b7de9e6
Author: James Bottomley <James.Bottomley@HansenPartnership.com>
Date:   Mon Jan 22 14:05:02 2018 -0800

    e_tpm2: Don't keep TPM key resident for the openssl key lifetime

But this was eliminated by the update, so now the TSS tries to write
keys to the current working directory of the application meaning that
if the application starts in an unwriteable location, all engine
actions fail.

Fix this by correctly setting the TPM_DATA_DIR property when the key
is loaded.

Fixes: a5dab4a412b800462b62092579ccedfc4b7de9e6
Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

e_tpm2.c
@@ -382,6 +382,11 @@ TPM_HANDLE tpm2_load_key(TSS_CONTEXT **tsscp, struct app_data *app_data)
 		tpm2_error(rc, "TSS_Create");
 		return 0;
 	}
+	rc = TSS_SetProperty(tssContext, TPM_DATA_DIR, app_data->dir);
+	if (rc) {
+		tpm2_error(rc, "TSS_SetProperty");
+		return 0;
+	}
 
 	buffer = app_data->priv;
 	size = app_data->priv_len;