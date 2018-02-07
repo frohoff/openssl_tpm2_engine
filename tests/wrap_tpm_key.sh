d94b6bfa698d2d5d9d180c5378f1623ac40170ae
James Bottomley <James.Bottomley@HansenPartnership.com>
2018-02-07 12:13:34 +0000

    test/wrap_tpm_key.sh: add check for unparented key creation
    If you don't specify the parent, we always do a TPM2_CreatePrimary on
    the storage seed.  Check that a key of this type (unnamed parent)
    functions correctly.
    
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- tests/wrap_tpm_key.sh
+++ tests/wrap_tpm_key.sh
@@ -11,8 +11,12 @@ bindir=${srcdir}/..
 # 4. encode a message using the TPM key
 # 5. verify the message through the public key
 ##
-openssl genrsa 2048 > key1.priv && \
-${bindir}/create_tpm2_key -p 81000001 -w key1.priv key1.tpm && \
-openssl rsa -engine tpm2 -inform engine -in key1.tpm -pubout -out key1.pub && \
-echo "This is another message" | openssl rsautl -sign -engine tpm2 -engine tpm2 -keyform engine -inkey key1.tpm -out tmp.msg && \
-openssl rsautl -verify -in tmp.msg -inkey key1.pub -pubin
+openssl genrsa 2048 > key1.priv || exit 1;
+for parent in "" "-p 81000001"; do
+    ${bindir}/create_tpm2_key ${parent} -w key1.priv key1.tpm || exit 1
+    openssl rsa -engine tpm2 -inform engine -in key1.tpm -pubout -out key1.pub || exit 1
+    echo "This is another message" | openssl rsautl -sign -engine tpm2 -engine tpm2 -keyform engine -inkey key1.tpm -out tmp.msg || exit 1
+    openssl rsautl -verify -in tmp.msg -inkey key1.pub -pubin || exit 1
+done
+
+exit 0
