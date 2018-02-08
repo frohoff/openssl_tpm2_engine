frohoff/openssl_tpm2_engine

commit 0af2c22093582a9c6e947fb1842e28dc671f3cea

tpm2-common: switch to using ECC parent keys
TPM2_CreatePrimary takes ages for RSA keys (45+ seconds on most
discrete TPMs), so switch to using Elliptic Curve primaries which can
be generated much faster.

Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

tpm2-common.c
@@ -246,7 +246,7 @@ TPM_RC tpm2_load_srk(TSS_CONTEXT *tssContext, TPM_HANDLE *h, const char *auth,TP
 	in.creationPCR.count = 0;
 
 	/* public parameters for an RSA2048 key  */
-	in.inPublic.publicArea.type = TPM_ALG_RSA;
+	in.inPublic.publicArea.type = TPM_ALG_ECC;
 	in.inPublic.publicArea.nameAlg = TPM_ALG_SHA256;
 	in.inPublic.publicArea.objectAttributes.val =
 		TPMA_OBJECT_NODA |
@@ -254,13 +254,13 @@ TPM_RC tpm2_load_srk(TSS_CONTEXT *tssContext, TPM_HANDLE *h, const char *auth,TP
 		TPMA_OBJECT_USERWITHAUTH |
 		TPMA_OBJECT_DECRYPT |
 		TPMA_OBJECT_RESTRICTED;
-	in.inPublic.publicArea.parameters.rsaDetail.symmetric.algorithm = TPM_ALG_AES;
-	in.inPublic.publicArea.parameters.rsaDetail.symmetric.keyBits.aes = 128;
-	in.inPublic.publicArea.parameters.rsaDetail.symmetric.mode.aes = TPM_ALG_CFB;
-	in.inPublic.publicArea.parameters.rsaDetail.scheme.scheme = TPM_ALG_NULL;
-	in.inPublic.publicArea.parameters.rsaDetail.keyBits = 2048;
-	/* means conventional 2^16+1 */
-	in.inPublic.publicArea.parameters.rsaDetail.exponent = 0;
+	in.inPublic.publicArea.parameters.eccDetail.symmetric.algorithm = TPM_ALG_AES;
+	in.inPublic.publicArea.parameters.eccDetail.symmetric.keyBits.aes = 128;
+	in.inPublic.publicArea.parameters.eccDetail.symmetric.mode.aes = TPM_ALG_CFB;
+	in.inPublic.publicArea.parameters.eccDetail.scheme.scheme = TPM_ALG_NULL;
+	in.inPublic.publicArea.parameters.eccDetail.curveID = TPM_ECC_NIST_P256;
+	in.inPublic.publicArea.parameters.eccDetail.kdf.scheme = TPM_ALG_NULL;
+
 	in.inPublic.publicArea.unique.rsa.t.size = 0;
 	in.inPublic.publicArea.authPolicy.t.size = 0;
 

commit 3c530ba60a458c612f130381e6a7f0d982affd31

tpm2-common: fix ECC template for primary key
There was still an rsa setting in the unique id.  The TSS fills this
in, but allows you to specify if it's non-zero.  Apparently this
problem causes a build failure on Debian presumably because the
on-stack data area doesn't contain zeros in this location.  Fix this
by making the ECC unique zero by setting both points to zero size.

Fixes: 0af2c22093582a9c6e947fb1842e28dc671f3cea
Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

tpm2-common.c
@@ -262,7 +262,8 @@ TPM_RC tpm2_load_srk(TSS_CONTEXT *tssContext, TPM_HANDLE *h, const char *auth,TP
 	in.inPublic.publicArea.parameters.eccDetail.curveID = TPM_ECC_NIST_P256;
 	in.inPublic.publicArea.parameters.eccDetail.kdf.scheme = TPM_ALG_NULL;
 
-	in.inPublic.publicArea.unique.rsa.t.size = 0;
+	in.inPublic.publicArea.unique.ecc.x.t.size = 0;
+	in.inPublic.publicArea.unique.ecc.y.t.size = 0;
 	in.inPublic.publicArea.authPolicy.t.size = 0;
 
 	rc = TSS_Execute(tssContext,