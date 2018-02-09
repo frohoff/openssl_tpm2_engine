ae4b8d3c7ca130bb7962954f72ed3a3d97c91fdc
James Bottomley <James.Bottomley@HansenPartnership.com>
2018-02-08 19:26:28 -0800

    tests: add prefix change to test_nv_key.sh
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- tests/test_nv_key.sh
+++ tests/test_nv_key.sh
@@ -34,14 +34,25 @@ echo "This is an internal key message" | openssl rsautl -sign -passin pass:${aut
 openssl rsautl -verify -in tmp.msg -inkey key1.pub -pubin || exit 1
 tssevictcontrol -hi o -hp ${nvkey} -ho ${nvkey}
 
-# and finally password plus DA implications
+# password plus DA implications
 tsscreate -rsa -gp -hp 81000001 -pwdk ${auth} -da -opr key.tpmpriv -opu key.tpmpub || exit 1
+key=$(tssload -hp 81000001 -ipu key.tpmpub -ipr key.tpmpriv|sed 's/Handle //') || exit 1
+tssevictcontrol -hi o -ho ${key} -hp ${nvkey} || exit 1
+tssflushcontext -ha ${key}
+openssl rsa -engine tpm2 -inform engine -passin pass:${auth} -in //nvkey:${nvkey} -pubout -out key1.pub || exit 1
+echo "This is an internal key message" | openssl rsautl -sign -passin pass:${auth} -engine tpm2 -engine tpm2 -keyform engine -inkey //nvkey:${nvkey} -out tmp.msg || exit 1
+openssl rsautl -verify -in tmp.msg -inkey key1.pub -pubin || exit 1
+tssevictcontrol -hi o -hp ${nvkey} -ho ${nvkey}
+
+# try with a different nvprefix
+tsscreate -rsa -gp -hp 81000001 -opr key.tpmpriv -opu key.tpmpub || exit 1
 key=$(tssload -hp 81000001 -ipu key.tpmpub -ipr key.tpmpriv|sed 's/Handle //') || exit 1
 tssevictcontrol -hi o -ho ${key} -hp ${nvkey} || exit 1
 tssflushcontext -ha ${key}
 
 openssl rsa -engine tpm2 -inform engine -passin pass:${auth} -in //nvkey:${nvkey} -pubout -out key1.pub || exit 1
-echo "This is an internal key message" | openssl rsautl -sign -passin pass:${auth} -engine tpm2 -engine tpm2 -keyform engine -inkey //nvkey:${nvkey} -out tmp.msg || exit 1
+export NVPREFIX="wibble:"
+echo "This is an internal key message" | openssl rsautl -sign -passin pass:${auth} -engine tpm2 -engine tpm2 -keyform engine -inkey ${NVPREFIX}${nvkey} -out tmp.msg || exit 1
 openssl rsautl -verify -in tmp.msg -inkey key1.pub -pubin || exit 1
 tssevictcontrol -hi o -hp ${nvkey} -ho ${nvkey}
 
