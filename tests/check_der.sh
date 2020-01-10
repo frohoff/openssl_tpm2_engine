1be01f3bf05298dbf441813a4946aed20b2229ab
James Bottomley <James.Bottomley@HansenPartnership.com>
2020-01-10 08:31:28 -0800

    tests: add a test for DER keys
    Now that we're supposed to support DER based keys, check that we can
    use them.
    
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

---
+++ tests/check_der.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+bindir=${srcdir}/..
+
+##
+# test is
+# 1. create TPM internal private key
+# 2. convert it to DER form
+# 3. get the corresponding public key from the engine
+# 4. encode a message using the TPM key
+# 5. verify the message through the public key
+${bindir}/create_tpm2_key -a -k passw0rd key2.tpm && \
+openssl asn1parse -in key2.tpm -out key2.der && \
+openssl rsa -engine tpm2 -inform engine -passin pass:passw0rd -in key2.der -pubout -out key2.pub && \
+echo "This is a message" | openssl rsautl -sign -engine tpm2 -engine tpm2 -keyform engine -inkey key2.der -passin pass:passw0rd -out tmp.msg && \
+openssl rsautl -verify -in tmp.msg -inkey key2.pub -pubin
+
